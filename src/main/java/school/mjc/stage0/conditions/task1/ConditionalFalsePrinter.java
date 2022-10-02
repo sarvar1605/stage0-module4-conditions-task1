9ea111742d89ed85b451eba0be500cef6836bb18
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-02 23:15:31 +0500

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if(true){
-            System.out.println("true");
+        if(false){
+        } else {
+           System.out.println("false");
         }
     }
 }
